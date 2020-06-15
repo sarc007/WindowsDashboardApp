71b942b3295b82674f2d09505c09802861ae9502
sarc007 <sarc007@yahoo.com>
2020-06-15 19:49:47 +0500

    Screen Resolutions with Height and Width

--- Form3.cs
+++ Form3.cs
@@ -455,16 +455,19 @@ namespace WindowsDashboardApp
         {
             foreach (var screen in System.Windows.Forms.Screen.AllScreens)
             {
-                listBox1.Items.Add("Device Name: " + screen.DeviceName);
-                listBox1.Items.Add("Bounds: " +
-                    screen.Bounds.ToString());
-                listBox1.Items.Add("Type: " +
-                    screen.GetType().ToString());
-                listBox1.Items.Add("Working Area: " +
-                    screen.WorkingArea.ToString());
-                listBox1.Items.Add("Primary Screen: " +
-                    screen.Primary.ToString());
+                //listBox1.Items.Add("Device Name: " + screen.DeviceName);
+                //listBox1.Items.Add("Bounds: " +
+                  //  screen.Bounds.ToString());
+                listBox1.Items.Add("Height :"+screen.Bounds.Height.ToString());
+                listBox1.Items.Add("Width :" + screen.Bounds.Width.ToString());
+                //listBox1.Items.Add("Type: " +
+                  //  screen.GetType().ToString());
+                //listBox1.Items.Add("Working Area: " +
+//                    screen.WorkingArea.ToString());
+  //              listBox1.Items.Add("Primary Screen: " +
+    //                screen.Primary.ToString());
             }
+          
         }
     }
 }
